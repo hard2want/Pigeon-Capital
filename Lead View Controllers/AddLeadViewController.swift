f8c86764e3c904e4ff76883626b8a96118e51485
hard2want <thedukeofchicago@gmail.com>
2018-08-06 11:02:58 -0500

    additional lead info added and functional

--- Lead View Controllers/AddLeadViewController.swift
+++ Lead View Controllers/AddLeadViewController.swift
@@ -17,7 +17,19 @@ class AddLeadViewController: UIViewController {
     @IBOutlet weak var stateTextField: UITextField!
     @IBOutlet weak var websiteTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
-
+    @IBOutlet weak var agentTextField: UITextField!
+    @IBOutlet weak var industryTextField: UITextField!
+    
+    @IBOutlet weak var referralSwitch: UISwitch!
+    @IBOutlet weak var strongReferralSwitch: UISwitch!
+    @IBOutlet weak var prioritySwitch: UISwitch!
+    @IBOutlet weak var email1SentSwitch: UISwitch!
+    @IBOutlet weak var responseSwitch: UISwitch!
+    @IBOutlet weak var toPrescreenSwitch: UISwitch!
+    
+    @IBOutlet weak var passSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,6 +39,16 @@ class AddLeadViewController: UIViewController {
             stateTextField.text = company.state
             websiteTextField.text = company.website
             descriptionTextField.text = company.description
+            agentTextField.text = company.agent
+            industryTextField.text = company.industry
+            referralSwitch.isOn = company.referral
+            strongReferralSwitch.isOn = company.strongReferral
+            prioritySwitch.isOn = company.priority
+            email1SentSwitch.isOn = company.email1Sent
+            responseSwitch.isOn = company.responseUnder24Hours
+            toPrescreenSwitch.isOn = company.preScreen
+            passSwitch.isOn = company.pass
+            
         } // end if let company = company
     } // end viewDidLoad()
 
@@ -41,15 +63,24 @@ class AddLeadViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
      super.prepare(for: segue, sender: sender)
      
-     guard segue.identifier == "saveUnwind" else {return}
+        guard segue.identifier == "saveUnwind" else {return}
      
-     let name = companyNameTextField.text ?? "missing company name"
-     let city = cityTextField.text ?? "missing city"
-     let state = stateTextField.text ?? "missing state"
-     let description = descriptionTextField.text ?? "missing description"
-     let website = websiteTextField.text ?? "missing website"
+        let name = companyNameTextField.text ?? "missing company name"
+        let city = cityTextField.text ?? "missing city"
+        let state = stateTextField.text ?? "missing state"
+        let description = descriptionTextField.text ?? "missing description"
+        let website = websiteTextField.text ?? "missing website"
+        let agent = agentTextField.text ?? "missing agent"
+        let industry = industryTextField.text ?? "missing industry"
+        let referral = referralSwitch.isOn
+        let strongReferral = strongReferralSwitch.isOn
+        let priority = prioritySwitch.isOn
+        let email1Sent = email1SentSwitch.isOn
+        let response = responseSwitch.isOn
+        let prescreen = toPrescreenSwitch.isOn
+        let pass = passSwitch.isOn
         
      
-     company = Company(name: name, description: description, city: city, state: state, website: website, industry: "", agent: "", referral: false, priority: false, email1Sent: false, responseUnder24Hours: false, moveToPreScreen: false, iot: false, midwest: false, leadInvestor: false, preSeedUnder6M: false, raiseUnder1M: false, founderRightPersonToAddressProblem: "", founderIsResponsive: "", ideaHasStrongRecurringRevenueComponent: "", teamCommittedFullTimePostClose: "", tractable: false, democratization: false, accessToIdleSupply: false, customerExpansion: false, networkEffects: false, bizDevInnovation: false, competeWithNonConsumption: false, thesisCounter: 0, founderStrengthIT: "", founderStrengthSales: "", founderStrengthMarketing: "", founderStrengthHustler: "", founderStrengthLearner: "", foundStoryStoryTellingAbility: "", foundDecisionMakingLogic: "", founderCustomerObsessed: "", founderDomainExpertise: "", teamStrongTechnicalBuilder: "", teamStrongBDLeader: "", clearProblemDefinition: "", largeGrowingTamSamSom: "", validRealMoat: "", strongBeachHead: "", ableToBridgeTamSam: "", tenYearTest: "", clearUserBuyer: "", signularFocusedModel: "", definedSuccessMetrics: "", favorableMarketTrend: "", customerBudgetLineItem: "", coreOfferingInHouse: "", highUserEngagement: "", highSomTraction: "", lowChurn: "", strongMarketingAdoptionPlan: "", fundingProvidesRunway: "", useCaseSupport: false, technicalDifferentiation: false, teamBackground: false, marketAnalysis: false, revenueModel: false, customerAcquisition: false, financialReview: false, riskAnalysis: false, charterDocuments: false, boardMinutes: false, stockCapTable: false, regulatory: false, intellectualProperty: false, managementEmployee: false, financialAudit: false, preScreen: false, scorecard: false, diligence: false, portfolio: false, pass: false)
+        company = Company(name: name, description: description, city: city, state: state, website: website, industry: industry, agent: agent, referral: referral, strongReferral: strongReferral, priority: priority, email1Sent: email1Sent, responseUnder24Hours: response, iot: false, midwest: false, leadInvestor: false, preSeedUnder6M: false, raiseUnder1M: false, founderRightPersonToAddressProblem: "", founderIsResponsive: "", ideaHasStrongRecurringRevenueComponent: "", teamCommittedFullTimePostClose: "", tractable: false, democratization: false, accessToIdleSupply: false, customerExpansion: false, networkEffects: false, bizDevInnovation: false, competeWithNonConsumption: false, thesisCounter: 0, founderStrengthIT: "", founderStrengthSales: "", founderStrengthMarketing: "", founderStrengthHustler: "", founderStrengthLearner: "", foundStoryStoryTellingAbility: "", foundDecisionMakingLogic: "", founderCustomerObsessed: "", founderDomainExpertise: "", teamStrongTechnicalBuilder: "", teamStrongBDLeader: "", clearProblemDefinition: "", largeGrowingTamSamSom: "", validRealMoat: "", strongBeachHead: "", ableToBridgeTamSam: "", tenYearTest: "", clearUserBuyer: "", signularFocusedModel: "", definedSuccessMetrics: "", favorableMarketTrend: "", customerBudgetLineItem: "", coreOfferingInHouse: "", highUserEngagement: "", highSomTraction: "", lowChurn: "", strongMarketingAdoptionPlan: "", fundingProvidesRunway: "", useCaseSupport: false, technicalDifferentiation: false, teamBackground: false, marketAnalysis: false, revenueModel: false, customerAcquisition: false, financialReview: false, riskAnalysis: false, charterDocuments: false, boardMinutes: false, stockCapTable: false, regulatory: false, intellectualProperty: false, managementEmployee: false, financialAudit: false, preScreen: prescreen, scorecard: false, diligence: false, portfolio: false, pass: pass)
      } // end prepare(for segue: )
 } // end AddLeadViewController
---
+++ Prescreen View Controllers/PreScreenTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PreScreenTableViewCell.swift
+//  Pigeon Capital
+//
+//  Created by The Duke on 8/6/18.
+//  Copyright © 2018 The Duke. All rights reserved.
+//
+
+import UIKit
+
+class PreScreenTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
